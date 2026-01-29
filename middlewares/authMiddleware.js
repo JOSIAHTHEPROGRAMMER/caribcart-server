1b278ed9e120f80f889b4741edca7204d26530ee
JOSIAHTHEPROGRAMMER <josiahalexis15@gmail.com>
2026-01-29 10:51:23 -0400

    Complete backend

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,40 @@
+import { clerkClient } from "@clerk/express";
+
+export const protect = async (req, res, next) => {
+    try {
+        const { userId, has } = await req.auth();
+
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        if (await has({ plan: 'enterprise' })) {
+            req.plan = 'enterprise';
+        } else if (await has({ plan: 'pro' })) {
+            req.plan = 'pro';
+        } else {
+            req.plan = 'basic';
+        }
+        return next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ message: error.code || error.message });
+    }
+};
+
+export const protectAdmin = async (req, res, next) => {
+    try {
+        const user = await clerkClient.users.getUser(await req.auth().userId);
+
+        const isAdmin = process.env.ADMIN_EMAILS.split(",").includes(user.emailAddresses[0].emailAddress);
+
+        if (!isAdmin) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        return next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ message: error.code || error.message });
+    }
+};
